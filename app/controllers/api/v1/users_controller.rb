ec927a67f2017e72031357a648239a920a127140
ever <ever864@gmail.com>
2022-02-24 01:22:34 -0500

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
 
-  def show 
-    render json: User.find(params[:id])
+  # GET /users/1
+  def show
+    render json: @user
   end
 
+  # Post /users
   def create
     @user = User.new(user_params)
 
@@ -14,9 +17,22 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
