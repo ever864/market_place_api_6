f88a74b4c5f3c6d01feb0e9f1d74486710c92b39
ever <ever864@gmail.com>
2022-04-17 00:33:59 -0500

    Adds search scopes on the product model

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -9,7 +9,7 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     get api_v1_user_url(@user), as: :json
     assert_response :success
 
-    json_response = JSON.parse( self.response.body, symbolize_names: true )
+    json_response = JSON.parse(response.body, symbolize_names: true)
     assert_equal @user.email, json_response.dig(:data, :attributes, :email)
     assert_equal @user.products.first.id.to_s, json_response.dig(:data, :relationships, :products, :data, 0, :id)
     assert_equal @user.products.first.title, json_response.dig(:include, 0, :attributes, :title)
