76931fc779fcdd374d37730dfa67bdc9e23fb4cb
ever <ever864@gmail.com>
2022-04-18 13:26:11 -0500

    Adds search class method to filter products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,5 +1,4 @@
 require "test_helper"
-
 class ProductTest < ActiveSupport::TestCase
   test 'should have a positive price' do
     product = products(:one)
@@ -27,7 +26,25 @@ class ProductTest < ActiveSupport::TestCase
 
   test 'should sort product by most recent' do
     products(:two).touch
-    assert_equal [products(:another_tv), products(:one),
-    products(:two)], Product.recent.to_a
+    assert_equal [products(:another_tv), products(:one), products(:two)], Product.recent.to_a
+  end
+
+  test 'search should not find "videogame" and "100" as min price' do
+    search_hash = { keyword: 'videogame', min_price: 100}
+    assert Product.search(search_hash).empty?
+  end
+
+  test 'search should find cheap TV' do
+    search_hash = { keyword: 'tv', min_price: 50, max_price: 150 }
+    assert_equal [products(:another_tv)], Product.search(search_hash)
+  end
+
+  test 'should get all products when no parameters' do
+    assert_equal Product.all.to_a, Product.search({})
+  end
+
+  test 'search should filter by product ids' do
+    search_hash = { products_ids: [products(:one).id] }
+    assert_equal [products(:one)], Product.search(search_hash)
   end
 end
